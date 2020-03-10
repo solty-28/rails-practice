91c737d7c5e5370c9c9c080d9488de606083cbd6
shioshi <soltyna28@gmail.com>
2020-03-10 22:31:24 +0900

    [Finish]3-8

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'cab52a600cb96dff67e815c5443ab4c155d387b4ffeddf1a69912043513af9ae786ea7f634acc079780a24434f1498f246416e836c3aed053ac8484b21e93433'
+
